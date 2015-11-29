e09952b49281d8d7a667d61c2ba2440f9f9b4434
Andriy Bilous <andrij.bilous@gmail.com>
2015-11-29 20:26:57 +0200

    creating shop from CSV improved, template file download added

--- shipping/csv_container/csv_agent_container.py
+++ shipping/csv_container/csv_agent_container.py
@@ -1,12 +1,17 @@
 import csv
+import os
+import datetime
 from beforeship.settings import SERVER_TMP_FOLDER
 
 
 class CSVFile():
 
-    def __init__(self, file_in_memory_obj):
+    def __init__(self, file_in_memory_obj, shop_name):
+        self.datetime = datetime.datetime.strftime(datetime.datetime.now(),
+                                                   '%Y_%m_%d_%H_%M_%S')
         self.file_in_memory_obj = file_in_memory_obj
-        self.file_path = SERVER_TMP_FOLDER + "tmp_file.csv"
+        self.file_path = SERVER_TMP_FOLDER + shop_name + self.datetime +\
+            ".csv"
         self.file_content_list = []
         self._save_file_on_server()
 
@@ -20,3 +25,4 @@ class CSVFile():
             self.csv_dict_reader = csv.DictReader(f)
             for order in self.csv_dict_reader:
                 self.file_content_list.append(order)
+        os.remove(self.file_path)
--- shipping/views/csv.py
+++ shipping/views/csv.py
@@ -20,58 +20,63 @@ class CSVInputView(FormView):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        form = self.get_form()
-        if form.is_valid():
-            shop, created =\
-                Shops.objects.get_or_create(owner=self.request.user,
-                                            shop_source=ShopSources.\
-                                                objects.get(source="CSV"),
-                                            shop_name=form.\
-                                                cleaned_data["shop_name"],
-                                            token="")
-            if created:
+        self.form = self.get_form()
+        if self.form.is_valid():
+            if self._verify_shop():
+                self.csv_file = CSVFile(request.FILES["csv_file"],
+                                        self.form.cleaned_data["shop_name"])
+                self.csv_file.process_csv_file()
+                self._save_orders()
                 messages.info(request, ("Your shop has connected to service "
-                                        "successfully"))
+                                        "successfully, all orders have been "
+                                        "fetched from file"))
             else:
-                messages.info(request, ("Shop is already connected to the "
-                                        "service"))
-            self.csv_file = CSVFile(request.FILES["csv_file"])
-            self.csv_file.process_csv_file()
-            for order in self.csv_file.file_content_list:
-                _, created = Order.objects.\
-                    get_or_create(shop_id=shop,
-                                  backer_id=order["backer_id"],
-                                  first_name=order["first_name"],
-                                  last_name=order["last_name"],
-                                  email=order["email"],
-                                  order_date=order["order_date"],
-                                  reward=order["reward"],
-                                  notes=order["notes"],
-                                  financial_status=order["financial_status"],
-                                  shipping_first_name=\
-                                    order["shipping_first_name"],
-                                  shipping_last_name=\
-                                    order["shipping_last_name"],
-                                    shipping_company=\
-                                        order["shipping_company"],
-                                    shipping_address_1=\
-                                        order["shipping_address_1"],
-                                    shipping_address_2=\
-                                        order["shipping_address_2"],
-                                                             shipping_city=order["shipping_city"],
-                                                             shipping_country=order["shipping_country"],
-                                                             shipping_state=order["shipping_state"],
-                                                             shipping_postal_code=order["shipping_postal_code"],
-                                                             shipping_phone=order["shipping_phone"],
-                                                             billing_first_name=order["billing_first_name"],
-                                                             billing_last_name=order["billing_last_name"],
-                                                             billing_company=order["billing_company"],
-                                                             billing_address_1=order["billing_address_1"],
-                                                             billing_address_2=order["billing_address_2"],
-                                                             billing_city=order["billing_city"],
-                                                             billing_country=order["billing_country"],
-                                                             billing_state=order["billing_state"],
-                                                             billing_postal_code=order["billing_postal_code"],
-                                                             billing_phone=order["billing_phone"])
+                messages.info(request, ("Shop is already exist. Try another "
+                                        "name for shop or delete existing"))
             return HttpResponseRedirect(reverse("user_home"))
-        return self.form_invalid(form)
+        return self.form_invalid(self.form)
+
+    def _verify_shop(self):
+        self.shop, created = (Shops.objects.get_or_create
+                              (owner=self.request.user,
+                               shop_source=\
+                                ShopSources.objects.get(source="CSV"),
+                               shop_name=self.form.\
+                                cleaned_data["shop_name"],
+                               token=""))
+        if created:
+            return True
+        return False
+
+    def _save_orders(self):
+        for order in self.csv_file.file_content_list:
+            (Order.objects.create
+                          (shop_id=self.shop,
+                           backer_id=order["backer_id"],
+                           first_name=order["first_name"],
+                           last_name=order["last_name"],
+                           email=order["email"],
+                           order_date=order["order_date"],
+                           reward=order["reward"],
+                           notes=order["notes"],
+                           financial_status=order["financial_status"],
+                           shipping_first_name=order["shipping_first_name"],
+                           shipping_last_name=order["shipping_last_name"],
+                           shipping_company=order["shipping_company"],
+                           shipping_address_1=order["shipping_address_1"],
+                           shipping_address_2=order["shipping_address_2"],
+                           shipping_city=order["shipping_city"],
+                           shipping_country=order["shipping_country"],
+                           shipping_state=order["shipping_state"],
+                           shipping_postal_code=order["shipping_postal_code"],
+                           shipping_phone=order["shipping_phone"],
+                           billing_first_name=order["billing_first_name"],
+                           billing_last_name=order["billing_last_name"],
+                           billing_company=order["billing_company"],
+                           billing_address_1=order["billing_address_1"],
+                           billing_address_2=order["billing_address_2"],
+                           billing_city=order["billing_city"],
+                           billing_country=order["billing_country"],
+                           billing_state=order["billing_state"],
+                           billing_postal_code=order["billing_postal_code"],
+                           billing_phone=order["billing_phone"]))
